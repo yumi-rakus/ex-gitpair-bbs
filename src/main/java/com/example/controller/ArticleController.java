42406b2845005ed8d5bc0360f587bb99902a7e20
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 13:54:34 +0900

    ArticleController.javaのinsert()を修正

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -16,40 +16,40 @@ import com.example.service.ArticleService;
 @Controller
 @RequestMapping("")
 public class ArticleController {
-	
+
 	@ModelAttribute
 	private ArticleForm ArticleForm() {
 		return new ArticleForm();
 	}
-	
+
 	@ModelAttribute
 	private CommentForm CommentForm() {
 		return new CommentForm();
 	}
-	
+
 	@Autowired
 	private ArticleService articleService;
-	
+
 	@RequestMapping("")
 	public String index(Model model) {
-		
+
 		List<Article> articleList = articleService.showList();
-		
-		if(articleList.isEmpty()) {
+
+		if (articleList.isEmpty()) {
 			model.addAttribute("articleNotExist", "記事が1件も存在しません。");
 		} else {
 			model.addAttribute("articleList", articleList);
 		}
-		
+
 		return "bbs";
 	}
-	
+
 	@RequestMapping("/insert")
-	public String insert(ArticleForm articleForm) {
-		Article article=new Article();
+	public String insert(ArticleForm articleForm, Model model) {
+		Article article = new Article();
 		article.setName(articleForm.getName());
 		article.setContent(articleForm.getContent());
 		articleService.insert(article);
-		return "articleList";
+		return index(model);
 	}
 }

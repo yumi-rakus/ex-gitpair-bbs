da50111e5e4f173aea865dd09ed9ffd26cea033a
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 11:50:11 +0900

    ArticleRepositiry.javaにfindAll()を追加

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,21 +1,68 @@
 package com.example.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
+import com.example.domain.Comment;
 
+@Repository
 public class ArticleRepository {
+	
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER
+	= (rs, i) -> {
+		
+		Article article = new Article();
+		
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+		
+		Comment comment = new Comment();
+		
+		comment.setId(rs.getInt("com_id"));
+		comment.setName(rs.getString("com_name"));
+		comment.setContent(rs.getString("com_content"));
+		comment.setArticleId(rs.getInt("article_id"));
+		
+		List<Comment> commentList = new ArrayList<>();
+		
+		commentList.add(comment);
+		
+		article.setCommentList(commentList);
+		
+		return article;
+	};
+	
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
-	public void insert(Article article) {
-		String sql="insert into articles (name,content) values (:name,:content)";
+	public List<Article> findAll(){
 		
-		SqlParameterSource param=new BeanPropertySqlParameterSource(article);
+		StringBuilder sql = new StringBuilder();
 		
-		template.update(sql,param);
+		sql.append("SELECT ");
+		sql.append("art.id AS id, ");
+		sql.append("art.name AS name, ");
+		sql.append("art.content AS content, ");
+		sql.append("com.id AS com_id, ");
+		sql.append("com.name AS com_name, ");
+		sql.append("com.content AS com_content, ");
+		sql.append("com.article_id AS article_id ");
+		sql.append("FROM articles AS art ");
+		sql.append("LEFT OUTER JOIN ");
+		sql.append("comments AS com ");
+		sql.append("ON art.id = com.article_id ");
+		sql.append("ORDER BY art.id desc, com.id;");
+		
+		List<Article> articleList = template.query(sql.toString(), ARTICLE_ROW_MAPPER);
+		
+		return articleList;
 	}
+
 }

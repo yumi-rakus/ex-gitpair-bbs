e8e6b6d665db4102ea06068ac4510e7cf627b959
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 12:22:45 +0900

    ArticleService.javaにshowList()を追加

--- src/main/java/com/example/service/ArticleService.java
+++ src/main/java/com/example/service/ArticleService.java
@@ -1,15 +1,57 @@
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.Article;
+import com.example.domain.Comment;
 import com.example.repository.ArticleRepository;
 
+@Service
+@Transactional
 public class ArticleService {
 	@Autowired
 	private ArticleRepository articleRepository;
 	
+	
+	public List<Article> showList() {
+		
+		List<Article> articleList = articleRepository.findAll();
+
+		Map<Integer, List<Comment>> commentMap = new LinkedHashMap<>();
+
+		for (Article article : articleList) { // commentMapにArrayListを格納
+			commentMap.put(article.getCommentList().get(0).getArticleId(), new ArrayList<Comment>());
+		}
+
+		for (Article article : articleList) { // commentMapにキー名articleIdでコメントリストにコメントを格納
+			List<Comment> commentList = commentMap.get(article.getCommentList().get(0).getArticleId());
+			commentList.add(article.getCommentList().get(0));
+		}
+
+		Map<Integer, Article> articleMap = new LinkedHashMap<>();
+
+		for (Article article : articleList) { // articleMapにArticleオブジェクトを格納
+			List<Comment> commentList = commentMap.get(article.getId());
+			article.setCommentList(commentList);
+			articleMap.put(article.getId(), article);
+		}
+
+		List<Article> distinctArticleList = new ArrayList<>(articleMap.values());
+
+		return distinctArticleList;
+		
+	}
+	
 	public void insert(Article article) {
 		articleRepository.insert(article);
 	}
+	
+	
 }

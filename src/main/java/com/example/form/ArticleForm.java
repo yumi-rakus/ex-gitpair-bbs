c889706553de28d95e15bfda80f984215aa36561
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 14:52:29 +0900

    ArticleForm.javaにバリデーションチェック機能を実装

--- src/main/java/com/example/form/ArticleForm.java
+++ src/main/java/com/example/form/ArticleForm.java
@@ -1,9 +1,16 @@
 package com.example.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class ArticleForm {
 
+	@NotBlank(message="名前を入力してください")
+	@Size(min=0, max=50, message="名前は50文字以内で入力してください")
 	private String name;
 
+	@NotBlank(message = "記事内容を入力してください")
+	@Size(min=0, max=140, message="記事内容は140文字以内で入力してください")
 	private String content;
 
 	// getter setter

6b9df30bdb4624d18bfeb80a0741ef34151a7c2d
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2020-07-13 12:15:10 +0900

    commentForm作成

--- src/main/java/com/example/form/CommentForm.java
+++ src/main/java/com/example/form/CommentForm.java
@@ -1,5 +1,39 @@
 package com.example.form;
 
 public class CommentForm {
+	private Integer articleId;
+	
+	private String name;
+	
+	private String content;
 
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "Comment [articleId=" + articleId + ", name=" + name + ", content=" + content + "]";
+	}
+	
 }

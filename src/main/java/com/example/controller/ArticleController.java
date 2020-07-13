20271ff980cc03247013c7db0a21596c1269b1ad
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 12:23:25 +0900

    ArticleController.javaにindex()を追加

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,6 +1,10 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +12,10 @@ import com.example.domain.Article;
 import com.example.form.ArticleForm;
 import com.example.service.ArticleService;
 
+@Controller
+@RequestMapping("")
 public class ArticleController {
+	
 	@ModelAttribute
 	private ArticleForm ArticleForm() {
 		return new ArticleForm();
@@ -16,6 +23,20 @@ public class ArticleController {
 	
 	@Autowired
 	private ArticleService articleService;
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		
+		List<Article> articleList = articleService.showList();
+		
+		if(articleList.isEmpty()) {
+			model.addAttribute("articleNotExist", "記事が1件も存在しません。");
+		} else {
+			model.addAttribute("articleList", articleList);
+		}
+		
+		return "bbs";
+	}
 	
 	@RequestMapping("/insert")
 	public String insert(ArticleForm articleForm) {

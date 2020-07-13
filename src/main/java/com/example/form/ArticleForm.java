d83d8ee301c10eed9ba752fe0fac41af3aa7021d
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 11:30:25 +0900

    ArticleForm.javaにフィールド変数、getter setter、toStringを追加

--- src/main/java/com/example/form/ArticleForm.java
+++ src/main/java/com/example/form/ArticleForm.java
@@ -2,4 +2,31 @@ package com.example.form;
 
 public class ArticleForm {
 
+	private String name;
+
+	private String content;
+
+	// getter setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "ArticleForm [name=" + name + ", content=" + content + "]";
+	}
+
 }

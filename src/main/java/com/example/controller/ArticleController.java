e356765ffc1eae74cc7af21816fc7fa665faa6b9
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2020-07-13 13:57:57 +0900

    コメント投稿機能作成

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
+import com.example.domain.Comment;
 import com.example.form.ArticleForm;
 import com.example.form.CommentForm;
 import com.example.service.ArticleService;
+import com.example.service.CommentService;
 
 @Controller
 @RequestMapping("")
@@ -29,6 +31,9 @@ public class ArticleController {
 	
 	@Autowired
 	private ArticleService articleService;
+	
+	@Autowired
+	private CommentService CommentService;
 	
 	@RequestMapping("")
 	public String index(Model model) {
@@ -51,5 +56,15 @@ public class ArticleController {
 		article.setContent(articleForm.getContent());
 		articleService.insert(article);
 		return "articleList";
+	}
+	
+	@RequestMapping("/postComment")
+	public String postComment(CommentForm commentForm,Model model) {
+		Comment comment=new Comment();
+		comment.setName(commentForm.getName());
+		comment.setContent(commentForm.getContent());
+		comment.setArticleId(commentForm.getArticleId());
+		CommentService.postComment(comment);
+		return index(model);
 	}
 }

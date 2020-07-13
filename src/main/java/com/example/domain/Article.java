6a586555e14eac30c1c12cd9fe9238f777733586
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-13 11:08:33 +0900

    Article.javaにフィールド変数、constructor、getter setter、toStringを追加

--- src/main/java/com/example/domain/Article.java
+++ src/main/java/com/example/domain/Article.java
@@ -1,5 +1,67 @@
 package com.example.domain;
 
+import java.util.List;
+
 public class Article {
+
+	private Integer id;
+
+	private String name;
+
+	private String content;
+
+	private List<Comment> commentList;
+
+	// constructor
+	public Article() {
+
+	}
+
+	public Article(Integer id, String name, String content, List<Comment> commentList) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.content = content;
+		this.commentList = commentList;
+	}
+
+	// getter setter
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Comment> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(List<Comment> commentList) {
+		this.commentList = commentList;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList + "]";
+	}
 
 }
